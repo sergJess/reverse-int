6d25ec3b9476cf735c1ba1e2ac4dcceade652ea9
sergJess <serg.fantasy@yandex.ru>
2021-01-14 21:30:58 +0300

    add solution of the task reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (typeof n === 'number') {
+        const stringRepresentation = Math.abs(n) + '';
+        let reversedNumber = '';
+        for (length = stringRepresentation.length, i = length - 1; i >= 0; i--) {
+            reversedNumber += stringRepresentation[i];
+        }
+        return reversedNumber;
+    }
+    return false;
 }
